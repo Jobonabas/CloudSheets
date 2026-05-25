3939bbbad097335cb91a8b721ebcd7d6cf8cf376
Jonas Gehrung <jg175@hdm-stuttgart.de>
2026-05-26 01:18:57 +0200

    feat(auth): add cognito to cdk backend stack

--- infra/hello-cdk/lib/backend-stack.ts
+++ infra/hello-cdk/lib/backend-stack.ts
@@ -1,8 +1,9 @@
-import { Stack, StackProps, RemovalPolicy, Duration } from 'aws-cdk-lib';
+import { Stack, StackProps, RemovalPolicy, Duration, CfnOutput } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import { DatabaseInstance, DatabaseInstanceEngine, PostgresEngineVersion, StorageType } from 'aws-cdk-lib/aws-rds';
 import { InstanceType, Vpc} from 'aws-cdk-lib/aws-ec2';
 import { SecurityGroup, Peer, Port, InstanceClass, InstanceSize} from 'aws-cdk-lib/aws-ec2';
+import { Mfa, OAuthScope, UserPool, UserPoolClient, UserPoolClientIdentityProvider} from 'aws-cdk-lib/aws-cognito'
 
 interface BackendStackConfig {
   environment: 'dev' | 'prod';
@@ -42,6 +43,60 @@ interface BackendStackConfig {
         multiAz: false, //no multi Availability Zones
         backupRetention: Duration.days(0), //backups stored for 0 days
         removalPolicy: config.environment === 'dev' ? RemovalPolicy.DESTROY : RemovalPolicy.RETAIN, //automatically delete db when stack is removed for dev
+      });
+
+      const userPool = new UserPool(this, 'BackendUserPool', {
+        userPoolName: 'cloudsheets-user-pool',
+        selfSignUpEnabled: true,
+        signInAliases:{
+          email: true
+        },
+        autoVerify: {email: true},
+        passwordPolicy: {
+          minLength: 12,
+          requireUppercase: true,
+          requireLowercase: true,
+          requireDigits: true,
+          requireSymbols: true,
+        },
+        mfa: Mfa.REQUIRED,
+        mfaSecondFactor: {
+          sms: false,
+          otp: true
+        },
+        removalPolicy: config.environment === 'dev' ? RemovalPolicy.DESTROY : RemovalPolicy.RETAIN,
+      });
+
+      const providerDomain = userPool.addDomain('CognitoDomain', {
+        cognitoDomain: {
+            domainPrefix: `cloudsheets-auth-${config.environment}`,
+        },
+      });
+
+      const userPoolClient = new UserPoolClient(this, 'BackendUserPoolClient', {
+        userPool,
+        supportedIdentityProviders: [
+          UserPoolClientIdentityProvider.COGNITO,
+        ],
+        oAuth: {
+          flows: {
+            authorizationCodeGrant: true,
+          },
+          scopes: [OAuthScope.OPENID, OAuthScope.EMAIL, OAuthScope.PROFILE],
+          callbackUrls: ['http://localhost:5173/'],
+          logoutUrls: ['http://localhost:5173/']
+        },
+      });
+
+      new CfnOutput(this, 'UserPoolId', {
+        value: userPool.userPoolId
+      });
+      
+      new CfnOutput(this, 'UserPoolClientId', {
+        value: userPoolClient.userPoolClientId,
+      });
+      new CfnOutput(this, 'CognitoDomainUrl', {
+        value: providerDomain.baseUrl(),
       });
    }
  }
